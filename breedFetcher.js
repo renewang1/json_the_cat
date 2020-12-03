a19cfa4df0012a693395ce006cec2d0f8a52acd2
Rene Wang <rene.wang1@hotmail.com>
2020-12-03 20:21:29 +0000

    edited files to correctly call callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,16 +2,15 @@ const request = require('request');
 
 
 const fetchBreedDescription = function(breed, callback) {
-  callback();
   request(('https://api.thecatapi.com/v1/images/search?breed_ids=' + breed.substring(0, 4)), (error, response, body) => {
     if (error) {
-      throw new Error(error);
+      callback(error, null);
     }
     const data = JSON.parse(body);
     if (data[0] === undefined) {
       console.log(`Requested breed ${breed} not found`);
     } else {
-      console.log(data[0]);
+      callback(null, data[0]);
     }
   });
 };
